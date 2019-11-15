7b6f2d4f393b435fec17e343d621ac924e2296b8
yegow <allanyego05@gmail.com>
2019-11-15 13:02:33 +0300

    Updated app.js, passed extended option to body-parser

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false,
+  extended: true,
 }));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/v1', apiRouter);
