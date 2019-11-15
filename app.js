ffe62e3ce871b2cf52ebd98c5f6228c4da218b89
yegow <allanyego05@gmail.com>
2019-11-15 13:16:32 +0300

    updated app.js

--- app.js
+++ app.js
@@ -22,10 +22,8 @@ const app = express();
 
 // app.use(boom());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/v1', apiRouter);
 app.use((_req, res) => {
